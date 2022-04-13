3f739b042edb5f51df346c7c9dd77ffa25ebe614
Benjamin Böcker <benjamin.boecker@palasthotel.de>
2022-04-13 23:10:57 +0200

    Added direct color accessors

--- Sources/NordTheme/NordTheme+SwiftUI.swift
+++ Sources/NordTheme/NordTheme+SwiftUI.swift
@@ -33,4 +33,22 @@ public extension NordTheme.Aurora {
 	}
 }
 
+public extension Color {
+	static let nord0 = NordTheme.PolarNight.nord0.swiftUIColor
+	static let nord1 = NordTheme.PolarNight.nord1.swiftUIColor
+	static let nord2 = NordTheme.PolarNight.nord2.swiftUIColor
+	static let nord3 = NordTheme.PolarNight.nord3.swiftUIColor
+	static let nord4 = NordTheme.SnowStorm.nord4.swiftUIColor
+	static let nord5 = NordTheme.SnowStorm.nord5.swiftUIColor
+	static let nord6 = NordTheme.SnowStorm.nord6.swiftUIColor
+	static let nord7 = NordTheme.Frost.nord7.swiftUIColor
+	static let nord8 = NordTheme.Frost.nord8.swiftUIColor
+	static let nord9 = NordTheme.Frost.nord9.swiftUIColor
+	static let nord10 = NordTheme.Frost.nord10.swiftUIColor
+	static let nord11 = NordTheme.Aurora.nord11.swiftUIColor
+	static let nord12 = NordTheme.Aurora.nord12.swiftUIColor
+	static let nord13 = NordTheme.Aurora.nord13.swiftUIColor
+	static let nord14 = NordTheme.Aurora.nord14.swiftUIColor
+	static let nord15 = NordTheme.Aurora.nord15.swiftUIColor
+}
 
--- Sources/NordTheme/NordTheme+UIKit.swift
+++ Sources/NordTheme/NordTheme+UIKit.swift
@@ -32,4 +32,22 @@ public extension NordTheme.Aurora {
 	}
 }
 
+public extension UIColor {
+	static let nord0 = NordTheme.PolarNight.nord0.color
+	static let nord1 = NordTheme.PolarNight.nord1.color
+	static let nord2 = NordTheme.PolarNight.nord2.color
+	static let nord3 = NordTheme.PolarNight.nord3.color
+	static let nord4 = NordTheme.SnowStorm.nord4.color
+	static let nord5 = NordTheme.SnowStorm.nord5.color
+	static let nord6 = NordTheme.SnowStorm.nord6.color
+	static let nord7 = NordTheme.Frost.nord7.color
+	static let nord8 = NordTheme.Frost.nord8.color
+	static let nord9 = NordTheme.Frost.nord9.color
+	static let nord10 = NordTheme.Frost.nord10.color
+	static let nord11 = NordTheme.Aurora.nord11.color
+	static let nord12 = NordTheme.Aurora.nord12.color
+	static let nord13 = NordTheme.Aurora.nord13.color
+	static let nord14 = NordTheme.Aurora.nord14.color
+	static let nord15 = NordTheme.Aurora.nord15.color
+}
 
